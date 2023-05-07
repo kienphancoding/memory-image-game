885bf04325a2259e27111db78aa483afe9353253
kienphancoding <trungkienmps@gmail.com>
2023-05-08 00:06:51 +0700

    Edit content

--- index.js
+++ index.js
@@ -1,141 +1,74 @@
-
 // other themes to add ?
 // bigger memory?
 
 var library = {
-  pokemon: [
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980129/memory/Pokemon/Mewtwo.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980142/memory/Pokemon/Mew.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980154/memory/Pokemon/Articuno.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980164/memory/Pokemon/Zapdos.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980175/memory/Pokemon/Moltres.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980186/memory/Pokemon/Eevee.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980129/memory/Pokemon/Mewtwo.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980142/memory/Pokemon/Mew.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980154/memory/Pokemon/Articuno.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980164/memory/Pokemon/Zapdos.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980175/memory/Pokemon/Moltres.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980186/memory/Pokemon/Eevee.png'
+  onepiece: [
+    "https://i.pinimg.com/564x/1e/9d/a9/1e9da917d663b848e3c8cc58465c8d53.jpg",
+    "https://i.pinimg.com/564x/71/17/5b/71175bf950d0d6a422bfdb2b38582163.jpg",
+    "https://i.pinimg.com/564x/1f/20/1a/1f201a37df730a8d1b65d064e23c8b04.jpg",
+    "https://i.pinimg.com/564x/86/6a/19/866a19a44aef78ef11d8015018ffb829.jpg",
+    "https://i.pinimg.com/564x/dc/83/10/dc8310fe234de55beca4f655bc47eb18.jpg",
+    "https://i.pinimg.com/564x/28/6c/f4/286cf4e8b8d2c477da5d16e3e6fbf543.jpg",
+    "https://i.pinimg.com/564x/e1/35/ec/e135ecfc9e17ea634a53c8c5b5dba663.jpg",
+    "https://i.pinimg.com/564x/8c/0c/e9/8c0ce9a2edc0eee3c50d86f47769a8c9.jpg",
+    "https://i.pinimg.com/564x/32/ef/fc/32effc9f259773dd9ff9dd3c05db8d5d.jpg",
+    "https://i.pinimg.com/736x/6b/88/e4/6b88e459f5c3ab91b850769b59ab9f37.jpg",
+    "https://i.pinimg.com/564x/1e/9d/a9/1e9da917d663b848e3c8cc58465c8d53.jpg",
+    "https://i.pinimg.com/564x/71/17/5b/71175bf950d0d6a422bfdb2b38582163.jpg",
+    "https://i.pinimg.com/564x/1f/20/1a/1f201a37df730a8d1b65d064e23c8b04.jpg",
+    "https://i.pinimg.com/564x/86/6a/19/866a19a44aef78ef11d8015018ffb829.jpg",
+    "https://i.pinimg.com/564x/dc/83/10/dc8310fe234de55beca4f655bc47eb18.jpg",
+    "https://i.pinimg.com/564x/28/6c/f4/286cf4e8b8d2c477da5d16e3e6fbf543.jpg",
+    "https://i.pinimg.com/564x/e1/35/ec/e135ecfc9e17ea634a53c8c5b5dba663.jpg",
+    "https://i.pinimg.com/564x/8c/0c/e9/8c0ce9a2edc0eee3c50d86f47769a8c9.jpg",
+    "https://i.pinimg.com/564x/32/ef/fc/32effc9f259773dd9ff9dd3c05db8d5d.jpg",
+    "https://i.pinimg.com/736x/6b/88/e4/6b88e459f5c3ab91b850769b59ab9f37.jpg",
   ],
-  starwars: [
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980981/memory/starwars/anakin%20skywalker.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981009/memory/starwars/luke%20skywalker.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981022/memory/starwars/Obi%20wann.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981054/memory/starwars/Han%20solo.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981074/memory/starwars/chewbacca.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981095/memory/starwars/yoda.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981117/memory/starwars/dark%20sidious.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981141/memory/starwars/dark%20vador.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981165/memory/starwars/padme.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981190/memory/starwars/leia.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980981/memory/starwars/anakin%20skywalker.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981009/memory/starwars/luke%20skywalker.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981022/memory/starwars/Obi%20wann.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981054/memory/starwars/Han%20solo.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981074/memory/starwars/chewbacca.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981095/memory/starwars/yoda.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981117/memory/starwars/dark%20sidious.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981141/memory/starwars/dark%20vador.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981165/memory/starwars/padme.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981190/memory/starwars/leia.jpg'
+  spyxfamily: [
+    "https://i.pinimg.com/736x/7f/e7/9b/7fe79bc0a8d3c7c15381b786bf32a72a.jpg",
+    "https://i.pinimg.com/564x/f9/1e/58/f91e58c27b25f0d6d0244d7a99de335e.jpg",
+    "https://i.pinimg.com/564x/c9/74/6a/c9746af7d96c269edde675402a41523e.jpg",
+    "https://i.pinimg.com/564x/80/68/41/806841ab5654f75eece455d1feec0165.jpg",
+    "https://i.pinimg.com/564x/79/65/88/796588e36b48dc705770537617cd9922.jpg",
+    "https://i.pinimg.com/564x/cb/19/6d/cb196d441ca94a33786eaaf49aed1826.jpg",
+    "https://i.pinimg.com/564x/83/c2/c2/83c2c201b20ecf00fc5ea26e8e27a815.jpg",
+    "https://i.pinimg.com/564x/05/f5/58/05f558f867b145588e3cb18c98156770.jpg",
+    "https://i.pinimg.com/564x/23/12/7f/23127f157ab409b046137e6c621abc4f.jpg",
+    "https://i.pinimg.com/564x/68/bc/08/68bc08b88101c934ca0f10fcf1793d68.jpg",
+    "https://i.pinimg.com/736x/7f/e7/9b/7fe79bc0a8d3c7c15381b786bf32a72a.jpg",
+    "https://i.pinimg.com/564x/f9/1e/58/f91e58c27b25f0d6d0244d7a99de335e.jpg",
+    "https://i.pinimg.com/564x/c9/74/6a/c9746af7d96c269edde675402a41523e.jpg",
+    "https://i.pinimg.com/564x/80/68/41/806841ab5654f75eece455d1feec0165.jpg",
+    "https://i.pinimg.com/564x/79/65/88/796588e36b48dc705770537617cd9922.jpg",
+    "https://i.pinimg.com/564x/cb/19/6d/cb196d441ca94a33786eaaf49aed1826.jpg",
+    "https://i.pinimg.com/564x/83/c2/c2/83c2c201b20ecf00fc5ea26e8e27a815.jpg",
+    "https://i.pinimg.com/564x/05/f5/58/05f558f867b145588e3cb18c98156770.jpg",
+    "https://i.pinimg.com/564x/23/12/7f/23127f157ab409b046137e6c621abc4f.jpg",
+    "https://i.pinimg.com/564x/68/bc/08/68bc08b88101c934ca0f10fcf1793d68.jpg",
   ],
   lotr: [
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981408/memory/lotr/gandalf.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981438/memory/lotr/sauron.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981469/memory/lotr/Aragorn.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981501/memory/lotr/legolas.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981535/memory/lotr/Gimli.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981603/memory/lotr/golum.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981645/memory/lotr/sam.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981686/memory/lotr/saroumane.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981738/memory/lotr/bilbo.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981802/memory/lotr/frodo.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981408/memory/lotr/gandalf.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981438/memory/lotr/sauron.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981469/memory/lotr/Aragorn.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981501/memory/lotr/legolas.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981535/memory/lotr/Gimli.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981603/memory/lotr/golum.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981645/memory/lotr/sam.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981686/memory/lotr/saroumane.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981738/memory/lotr/bilbo.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547981802/memory/lotr/frodo.jpg'
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981408/memory/lotr/gandalf.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981438/memory/lotr/sauron.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981469/memory/lotr/Aragorn.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981501/memory/lotr/legolas.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981535/memory/lotr/Gimli.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981603/memory/lotr/golum.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981645/memory/lotr/sam.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981686/memory/lotr/saroumane.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981738/memory/lotr/bilbo.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981802/memory/lotr/frodo.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981408/memory/lotr/gandalf.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981438/memory/lotr/sauron.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981469/memory/lotr/Aragorn.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981501/memory/lotr/legolas.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981535/memory/lotr/Gimli.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981603/memory/lotr/golum.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981645/memory/lotr/sam.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981686/memory/lotr/saroumane.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981738/memory/lotr/bilbo.jpg",
+    "https://res.cloudinary.com/beumsk/image/upload/v1547981802/memory/lotr/frodo.jpg",
   ],
-  disney: [
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982044/memory/disney/mickey.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982088/memory/disney/mowgli.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982610/memory/disney/tarzan.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982620/memory/disney/simba.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982628/memory/disney/aladin.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982636/memory/disney/blanche%20neige.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982644/memory/disney/alice.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982653/memory/disney/peter%20pan.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982663/memory/disney/pinocchio.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982738/memory/disney/raiponce.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982044/memory/disney/mickey.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982088/memory/disney/mowgli.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982610/memory/disney/tarzan.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982620/memory/disney/simba.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982628/memory/disney/aladin.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982636/memory/disney/blanche%20neige.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982644/memory/disney/alice.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982653/memory/disney/peter%20pan.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982663/memory/disney/pinocchio.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982738/memory/disney/raiponce.jpg'
-  ],
-  pixar: [
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982971/memory/pixar/up.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982987/memory/pixar/buzz.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983000/memory/pixar/woody.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983016/memory/pixar/Remy.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983032/memory/pixar/Mike.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983077/memory/pixar/Nemo.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983114/memory/pixar/wall-e.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983169/memory/pixar/Mr-Incredible.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983381/memory/pixar/sully.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983403/memory/pixar/flash%20mcqueen.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982971/memory/pixar/up.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547982987/memory/pixar/buzz.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983000/memory/pixar/woody.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983016/memory/pixar/Remy.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983032/memory/pixar/Mike.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983077/memory/pixar/Nemo.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983114/memory/pixar/wall-e.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983169/memory/pixar/Mr-Incredible.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983381/memory/pixar/sully.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547983403/memory/pixar/flash%20mcqueen.jpg'
-  ],
-  harrypotter: [
-    'https://res.cloudinary.com/beumsk/image/upload/v1547998926/memory/harrypotter/harry.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547998958/memory/harrypotter/ron.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547998992/memory/harrypotter/hermione.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999106/memory/harrypotter/dumbledore.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999032/memory/harrypotter/malfoy.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999143/memory/harrypotter/voldemort.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999401/memory/harrypotter/rogue.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999196/memory/harrypotter/hagrid.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999271/memory/harrypotter/sirius.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999577/memory/harrypotter/neville.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547998926/memory/harrypotter/harry.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547998958/memory/harrypotter/ron.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547998992/memory/harrypotter/hermione.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999106/memory/harrypotter/dumbledore.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999032/memory/harrypotter/malfoy.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999143/memory/harrypotter/voldemort.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999401/memory/harrypotter/rogue.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999196/memory/harrypotter/hagrid.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999271/memory/harrypotter/sirius.jpg',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547999577/memory/harrypotter/neville.jpg'
-  ]
-}
+};
 
 var images = [];
 var tempElt1 = "";
@@ -155,27 +88,27 @@ var postElt = document.querySelector("#post");
 var finalElt = document.querySelector("#final");
 var againElt = document.querySelector("#again");
 
-
 // initiate the game with chosen theme
-themesElt.addEventListener("click", function(e) {
+themesElt.addEventListener("click", function (e) {
   if (e.target.classList.contains("themes")) {
     activateTheme(e.target.id);
     preElt.classList.add("hidden");
   }
 });
 
-
 function activateTheme(theme) {
   // insert theme in images array
-  for (let i = 0; i < 20; i++) {images.push(library[theme][i]);}  
+  for (let i = 0; i < 20; i++) {
+    images.push(library[theme][i]);
+  }
   // insert images in memory game
   for (let i = 0; i < 20; i++) {
     var rand = Math.floor(Math.random() * (images.length - 1));
-    boxElts[i].innerHTML = "<img src='" + images[rand] + "' alt='image' class='hidden'>";
+    boxElts[i].innerHTML =
+      "<img src='" + images[rand] + "' alt='image' class='hidden'>";
     images.splice(rand, 1);
   }
 }
-
 
 // Handle the play
 mainElt.addEventListener("click", gameLogic);
@@ -189,7 +122,7 @@ function gameLogic(e) {
       tempElt1 = e.target;
       // timer
       if (click === -1) {
-        timer = setInterval(function() {
+        timer = setInterval(function () {
           time++;
           timeElt.innerHTML = time;
         }, 1000);
@@ -204,7 +137,7 @@ function gameLogic(e) {
       // different images
       if (tempElt1.firstChild.src !== tempElt2.firstChild.src) {
         mainElt.removeEventListener("click", gameLogic);
-        setTimeout( function() {
+        setTimeout(function () {
           tempElt1.firstChild.classList.add("hidden");
           tempElt2.firstChild.classList.add("hidden");
           mainElt.addEventListener("click", gameLogic);
@@ -228,7 +161,8 @@ function gameLogic(e) {
         // game won
         if (win === 20) {
           clearInterval(timer);
-          finalElt.innerHTML = "You won " + score + " points <br> in " + time + " seconds";
+          finalElt.innerHTML =
+            "You won " + score + " points <br> in " + time + " seconds";
           postElt.classList.remove("hidden");
         }
       }
